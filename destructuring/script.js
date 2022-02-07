2d1ed16d877e861e6e2951c66cbfcd357221789f
AndyBeable <andy.beable@gmail.com>
2022-02-07 11:13:11 +0000

    initial destructuring

---
+++ destructuring/script.js
@@ -0,0 +1,10 @@
+const array = ['A', 'B', 'C', 'D']
+const person = {
+    name: "Andy",
+    age: 33,
+    favouriteFood: 'Pizza',
+    address: {
+        street: '123 Main Street',
+        city: 'London'
+    }
+}
