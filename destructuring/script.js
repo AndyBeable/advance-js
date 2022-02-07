d3daed252d6ef956143c7f9ccf5cb9488ed70d9c
AndyBeable <andy.beable@gmail.com>
2022-02-07 11:24:17 +0000

    array destructuring

--- destructuring/script.js
+++ destructuring/script.js
@@ -8,3 +8,23 @@ const person = {
         city: 'London'
     }
 }
+
+// ARRAY DESTRUCTURING
+
+const [first, second, ,fourth] = array
+console.log('destructured', first)
+console.log('destructured',second)
+console.log('destructured',fourth)
+
+function addAndMultiply(a, b) {
+    return [a + b, a * b]
+}
+
+const [sum, product] = addAndMultiply(4, 5)
+console.log(sum)
+console.log(product)
+
+const firstInArray = array[0]
+const secondInArray = array[1]
+console.log(firstInArray)
+console.log(secondInArray)
